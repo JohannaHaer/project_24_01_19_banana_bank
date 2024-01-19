b6f7adde05fbb65864f27e986a06c82bc26a1d40
JohannaHaer <johanna.haerdter@gmail.com>
2024-01-19 16:30:17 +0100

    funktionen fertig und doppelt

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,12 @@ import { useState } from 'react'
 import './App.css'
 import Account from './assets/components/account/Account'
 
-// function App() {
-  
-//   const [obj, setObj] = useState({
-//     entry: "",
-//     account: ""
-//   }) 
-
-//   return (
-//     <>
-//       <h1>Banana Bank</h1>
-//       <Account
-//         obj = {obj}
-//         setObj = {setObj}
-//       />
-//     </>
-//   )
-// }
-
-// export default App
-
-
 function App() {
   
   const [obj, setObj] = useState({
     entry: "",
-  })
-
-  const [account, setAccount] = useState(0)
-  
+    account: ""
+  }) 
 
   return (
     <>
@@ -39,12 +16,35 @@ function App() {
       <Account
         obj = {obj}
         setObj = {setObj}
-        account = {account}
-        setAccount = {setAccount}
       />
     </>
   )
 }
 
 export default App
+
+
+// function App() {
+  
+//   const [obj, setObj] = useState({
+//     entry: "",
+//   })
+
+//   const [account, setAccount] = useState(0)
+  
+
+//   return (
+//     <>
+//       <h1>Banana Bank</h1>
+//       <Account
+//         obj = {obj}
+//         setObj = {setObj}
+//         account = {account}
+//         setAccount = {setAccount}
+//       />
+//     </>
+//   )
+// }
+
+// export default App
 
--- src/assets/components/account/Account.jsx
+++ src/assets/components/account/Account.jsx
@@ -1,82 +1,40 @@
-// const Account = ({obj, setObj}) => {
-
-//     const depositFunc = () => {
-        
-//         let accountNum = Number(obj.account)
-//         console.log(accountNum);
-//         let entryNum = Number(obj.entry)
-//         console.log(entryNum);
-//         let addObj = {...obj, account: accountNum + entryNum, entry: ""}
-//         console.log(typeof addObj);
-//         setObj(addObj)
-        
-//     }
-
-//     const payOutFunc = () => {
-//         let accountNum = Number(obj.account)
-//         let entryNum = Number(obj.entry)
-//         let addObj = {...obj, account: accountNum - entryNum, entry: ""}
-//         setObj(addObj)
-//         if (addObj < 0) {
-//             window.alert("Du kannst nicht mehr abheben, als du besitzt")
-//             addObj = accountNum
-//         } 
-//         setObj(addObj)
-//     }
-
-
-//     return (
-//         <>
-//             <p>{obj.account} €</p>
-//             <div>
-//                 <input 
-//                 type="number" 
-//                 id = "input"
-//                 placeholder="Betrag in €"
-//                 value = {obj.entry}
-//                 onChange={(event) => setObj({...obj, entry: event.target.value})}
-//                 />
-//                 <button onClick={depositFunc}>Einzahlen</button>
-//                 <button onClick={payOutFunc}>Auszahlen</button>
-//             </div>
-
-//         </>
-//     );
-// }
-
-// export default Account;
-
-
-
-const Account = ({obj, setObj, account, setAccount}) => {
+const Account = ({obj, setObj}) => {
 
     const depositFunc = () => {
-        let accountNum = Number(account)
+        
+        let accountNum = Number(obj.account)
+        console.log(accountNum);
         let entryNum = Number(obj.entry)
-        let addObj = accountNum + entryNum
-        obj.entry = ""
-        setAccount(addObj)
+        console.log(entryNum);
+        let addObj = {...obj, account: accountNum + entryNum, entry: ""}
+        console.log(typeof addObj);
+        setObj(addObj)
+        
     }
 
     const payOutFunc = () => {
-        let accountNum = Number(account)
+        let accountNum = Number(obj.account)
         let entryNum = Number(obj.entry)
-        let addObj = accountNum - entryNum
-        obj.entry = ""
-        
-        if (addObj < 0) {
-            window.alert("Du kannst nicht mehr abheben, als du besitzt")
-            addObj = accountNum
-        } 
-        setAccount(addObj)
+        let addObj = {...obj, account: accountNum - entryNum, entry: ""}
+        setObj(addObj)
+
+        if (addObj.account < 0 && addObj.account > -1000) {
+               window.alert(`Du bist in deinem Dispo mit ${addObj.account}€ von 1000€ Limit`)
+        } else if (addObj.account < -1000) {
+                    window.alert("Du kannst dein Limit von 1000€ nicht überschreiten!")
+                    addObj.account = accountNum
+        }
+        setObj(addObj)
     }
+
 
     return (
         <>
-            <p>{account} €</p>
+            <p>{obj.account} €</p>
             <div>
                 <input 
                 type="number" 
+                id = "input"
                 placeholder="Betrag in €"
                 value = {obj.entry}
                 onChange={(event) => setObj({...obj, entry: event.target.value})}
@@ -90,3 +48,50 @@ const Account = ({obj, setObj, account, setAccount}) => {
 }
 
 export default Account;
+
+
+
+// const Account = ({obj, setObj, account, setAccount}) => {
+
+//     const depositFunc = () => {
+//         let accountNum = Number(account)
+//         let entryNum = Number(obj.entry)
+//         let addObj = accountNum + entryNum
+//         obj.entry = ""
+//         setAccount(addObj)
+//     }
+
+//     const payOutFunc = () => {
+//         let accountNum = Number(account)
+//         let entryNum = Number(obj.entry)
+//         let addObj = accountNum - entryNum
+//         obj.entry = ""
+        
+//         if (addObj < 0 && addObj > -1000) {
+//             window.alert(`Du bist in deinem Dispo mit ${addObj}€ von 1000€ Limit`)
+//         } else if (addObj < -1000) {
+//             window.alert("Du kannst dein Limit von 1000€ nicht überschreiten!")
+//             addObj = accountNum
+//         } 
+//         setAccount(addObj)
+//     }
+
+//     return (
+//         <>
+//             <p>{account} €</p>
+//             <div>
+//                 <input 
+//                 type="number" 
+//                 placeholder="Betrag in €"
+//                 value = {obj.entry}
+//                 onChange={(event) => setObj({...obj, entry: event.target.value})}
+//                 />
+//                 <button onClick={depositFunc}>Einzahlen</button>
+//                 <button onClick={payOutFunc}>Auszahlen</button>
+//             </div>
+
+//         </>
+//     );
+// }
+
+// export default Account;
